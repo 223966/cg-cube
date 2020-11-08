2e034e93c1dc2ed50d87a0fe5ab7e8c16d7fc0d7
Jan Lewandoski <janek.lewandoski@gmail.com>
2020-11-08 21:14:49 +0100

    Create Cube

--- Template1/Template1.cpp
+++ Template1/Template1.cpp
@@ -1,6 +1,3 @@
-// Template1.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
 #include <Windows.h>
 #include <math.h>
@@ -8,222 +5,97 @@
 #include "include\GL\GLU.H"
 #include "include\GL\glut.h"
 
-// You can change these parameters
-const double TRIANGLE_LENGTH = 80.0f;
-const double SPREAD_STEP = 1;
-const double SPREAD_LIMIT = 100.0;
+#define WIDTH 640
+#define HEIGHT 480
 
-const bool shouldRotateAroundCenterOfMass = true;
-const bool shouldRotateAroundOrigin = true;
-const bool shouldSpread = true;
+const float RED[3] = { 1.0f, 0.0f, 0.0f };
+const float GREEN[3] = { 0.0f, 1.0f, 0.0f };
+const float BLUE[3] = { 0.0f, 0.0f, 1.0f };
+const float MAGENTA[3] = { 255.0f, 0.0f, 255.0f };
+const float YELLOW[3] = { 255.0f, 255.0f, 0.0f };
 
-// ******************************* //
 
-const double OFFSET = TRIANGLE_LENGTH * 1.25;
+float ver[8][3] =
+{
+    {-1.0,-1.0,1.0},
+    {-1.0,1.0,1.0},
+    {1.0,1.0,1.0},
+    {1.0,-1.0,1.0},
+    {-1.0,-1.0,-1.0},
+    {-1.0,1.0,-1.0},
+    {1.0,1.0,-1.0},
+    {1.0,-1.0,-1.0},
+};
 
-const enum Color { red, green, blue, yellow };
-const enum Direction {left, right};
-const enum Position {bottomOuter, bottomInner, centerInner, centerOuter, topInner, topOuter};
-
-double angle = 0.0;
-double spread = 0.0;
-bool shouldIncrease = true;
-
-void timer(int) {
-	angle += 180 / SPREAD_LIMIT * SPREAD_STEP;
-	spread += shouldIncrease ? SPREAD_STEP : -SPREAD_STEP;
-	if (spread >= SPREAD_LIMIT) shouldIncrease = false;
-	if (spread <= 00.0) shouldIncrease = true;
-	glutPostRedisplay();
+void drawQuad(const float color[3], int a, int b, int c, int d)
+{
+    glBegin(GL_QUADS);
+    glColor3fv(color);
+    glVertex3fv(ver[a]);
+    glVertex3fv(ver[b]);
+    glVertex3fv(ver[c]);
+    glVertex3fv(ver[d]);
+    glEnd();
 }
 
-void draw() {
-	glBegin(GL_POLYGON);
-	glVertex2f(-TRIANGLE_LENGTH, 0);
-	glVertex2f(0, 0);
-	glVertex2f(0, TRIANGLE_LENGTH);
-	glEnd();
+void drawCube() {
+    drawQuad(RED, 0, 3, 2, 1);
+    drawQuad(GREEN, 2, 3, 7, 6);
+    drawQuad(BLUE, 0, 4, 7, 3);
+    drawQuad(BLUE, 1, 2, 6, 5);
+    drawQuad(YELLOW, 4, 5, 6, 7);
+    drawQuad(MAGENTA, 0, 1, 5, 4);
 }
 
-void drawRed() {
-	glColor4f(1.0f, 0.0f, 0.0f, 1.0f);//Red;
-	draw();
+double rotate_y = 0;
+double rotate_x = 0;
+void specialKeys(int key, int x, int y)
+{
+    if (key == GLUT_KEY_RIGHT)
+        rotate_x += 5;
+    else if (key == GLUT_KEY_LEFT)
+        rotate_x -= 5;
+    else if (key == GLUT_KEY_UP)
+        rotate_y += 5;
+    else if (key == GLUT_KEY_DOWN)
+        rotate_y -= 5;
+    glutPostRedisplay();
 }
 
-void drawGreen() {
-	glColor4f(0.0f, 1.0f, 0.0f, 1.0f);//Green;
-	draw();
+void display()
+{
+    glClearColor(0, 0, 0, 1);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(60, WIDTH / HEIGHT, 0.1, 100);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt
+    (
+       5, 5, 5,
+        0, 0, 0,
+        0, 0, 1
+    );
+
+    glRotated(rotate_x, 1.0, 0.0, 0.0);
+    glRotated(rotate_y, 0.0, 1.0, 0.0);
+    drawCube();
+
+    glutSwapBuffers();
 }
 
-void drawBlue() {
-	glColor4f(0.0f, 0.0f, 1.0f, 1.0f);//Blue;
-	draw();
-}
-
-void drawYellow() {
-	glColor4f(240.0f, 255.0f, 0.0f, 1.0f);//Yellow;
-	draw();
-}
-
-
-void drawTriangle(Color color) {
-	switch (color) {
-	case red: drawRed(); break;
-	case green: drawGreen(); break;
-	case blue: drawBlue(); break;
-	case yellow: drawYellow(); break;
-	}
-}
-
-void rotateAroundOrigin(Direction direction) {
-	switch (direction) {
-	case left: 	glRotated(angle, 0, 0, 1); break;
-	case right: glRotated(-angle, 0, 0, 1); break;
-	}
-}
-
-void rotateAroundCenterOfMass(Direction direction) {
-	if (direction == left) {
-		glTranslated(-TRIANGLE_LENGTH / 3, TRIANGLE_LENGTH / 3, 0);
-		glRotated(angle, 0, 0, 1);
-		glTranslated(TRIANGLE_LENGTH / 3, -TRIANGLE_LENGTH / 3, 0);
-	}
-	else {
-		glTranslated(-TRIANGLE_LENGTH / 3, TRIANGLE_LENGTH / 3, 0);
-		glRotated(-angle, 0, 0, 1);
-		glTranslated(TRIANGLE_LENGTH / 3, -TRIANGLE_LENGTH / 3, 0);
-	}
-	
-}
-
-void spreadOut(Position position) {
-	if(position == centerInner) glTranslated(-spread/3, spread/3, 0);
-	if(position == centerOuter) glTranslated(-spread *4/3, spread*4/3, 0);
-	if(position == bottomInner) glTranslated(-spread*4/3, spread/3, 0);
-	if(position == bottomOuter) glTranslated(-spread*7/3, spread/3, 0);
-	if(position == topInner) glTranslated(-spread/3, spread*4/3, 0);
-	if(position == topOuter) glTranslated(-spread/3, spread*7/3, 0);	
-}
-
-void drawQuarter(Color color) {
-	glPushMatrix();
-	if (shouldRotateAroundOrigin) rotateAroundOrigin(left);
-	if (shouldSpread) spreadOut(centerInner);
-	if (shouldRotateAroundCenterOfMass) rotateAroundCenterOfMass(left);
-	drawTriangle(color);
-	glPopMatrix();
-	glTranslated(-OFFSET, 0, 0);
-	glPushMatrix();
-	if (shouldRotateAroundOrigin) {
-		glTranslated(OFFSET, 0, 0);
-		rotateAroundOrigin(right);
-		glTranslated(-OFFSET, 0, 0);
-	} 
-	if (shouldSpread) spreadOut(bottomInner);
-	if (shouldRotateAroundCenterOfMass) rotateAroundCenterOfMass(right);
-	drawTriangle(color);
-	glPopMatrix();
-	glTranslated(-OFFSET, 0, 0);
-	glPushMatrix();
-	if (shouldRotateAroundOrigin) {
-		glTranslated(OFFSET*2, 0, 0);
-		rotateAroundOrigin(left);
-		glTranslated(-OFFSET*2, 0, 0);
-	}
-	if (shouldSpread) spreadOut(bottomOuter);
-	if (shouldRotateAroundCenterOfMass) rotateAroundCenterOfMass(left);
-	drawTriangle(color);
-	glPopMatrix();
-	glTranslated(OFFSET, OFFSET, 0);
-	glPushMatrix();
-	if (shouldRotateAroundOrigin) {
-		glTranslated(OFFSET, -OFFSET, 0);
-		rotateAroundOrigin(left);
-		glTranslated(-OFFSET, OFFSET, 0);
-	}
-	if (shouldSpread) spreadOut(centerOuter);
-	if (shouldRotateAroundCenterOfMass) rotateAroundCenterOfMass(left);
-	drawTriangle(color);
-	glPopMatrix();
-	glTranslated(OFFSET, 0, 0);
-	glPushMatrix();
-	if (shouldRotateAroundOrigin) {
-		glTranslated(0, -OFFSET, 0);
-		rotateAroundOrigin(right);
-		glTranslated(0, OFFSET, 0);
-	}
-	if (shouldSpread) spreadOut(topInner);
-	if (shouldRotateAroundCenterOfMass) rotateAroundCenterOfMass(right);
-	drawTriangle(color);
-	glPopMatrix();
-	glTranslated(0, OFFSET, 0);
-	glPushMatrix();
-	if (shouldRotateAroundOrigin) {
-		glTranslated(0, -OFFSET*2, 0);
-		rotateAroundOrigin(left);
-		glTranslated(0, OFFSET*2, 0);
-	}
-	if (shouldSpread) spreadOut(topOuter);
-	if (shouldRotateAroundCenterOfMass) rotateAroundCenterOfMass(left);
-	drawTriangle(color);
-	glPopMatrix();
-}
-
-void drawFigure() {
-	glPushMatrix();
-	glRotated(0, 0, 0, 0);
-	drawQuarter(red);
-	glPopMatrix();
-	glPushMatrix();
-	glRotated(180, 0, 0, 1);
-	drawQuarter(green);
-	glPopMatrix();
-	glPushMatrix();
-	glRotated(90, 0, 0, 1);
-	drawQuarter(yellow);
-	glPopMatrix();
-	glPushMatrix();
-	glRotated(-90, 0, 0, 1);
-	drawQuarter(blue);
-	glPopMatrix();
-}
-
-void MyDisplay(void) {
-	// The new scene
-	glLoadIdentity();
-	glClear(GL_COLOR_BUFFER_BIT);
-	glShadeModel(GL_SMOOTH);
-
-	glLoadIdentity();
-
-
-	drawFigure();
-
-	// The end of scene
-	glFlush();//start processing buffered OpenGL routines
-
-	glutTimerFunc(0.1, timer, 0);
-
-}
-void MyInit(void) {
-	glClearColor(0.0, 0.0, 0.0, 0.0);//select clearing (background) color
-									 /* initialize viewing values */
-	glViewport(0, 0, 300, 300);//window origin and size
-	glMatrixMode(GL_PROJECTION);//
-	glLoadIdentity();//=1
-	gluOrtho2D(-500.0, 500.0, -500.0, 500.0);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();//=1
-}
-
-int main(int argc, char** argv) { //<- for normal API
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);//single buffer and RGBA
-	glutInitWindowSize(500, 500);//initial window size
-	glutInitWindowPosition(100, 100);
-	glutCreateWindow("Triangles Excercise");//create widnow, hello title bar
-	MyInit();
-	glutDisplayFunc(MyDisplay);//
-	glutMainLoop();//enter main loop and process events
-	return 0;
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
+    glutInitWindowSize(WIDTH, HEIGHT);
+    glutCreateWindow("GLUT");
+    glutDisplayFunc(display);
+    glutSpecialFunc(specialKeys);
+    glEnable(GL_DEPTH_TEST);
+    glutMainLoop();
+    return 0;
 }
